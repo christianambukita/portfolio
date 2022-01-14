4620129e81af84ba2a3e30f6950a1eb1207d7b4f
Christian Ambukita <christianambukita@gmail.com>
2022-01-14 14:37:20 +0100

    Add black fill to canvas

--- src/index.js
+++ src/index.js
@@ -3,41 +3,66 @@ const ctx = canvas.getContext('2d');
 let { width: canvasWidth, height: canvasHeight } = canvas;
 ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 let id = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
-
+const canvasFps = 60;
+const canvasPos = {
+	top: undefined,
+	left: undefined,
+};
 const mousePos = {
 	x: undefined,
 	y: undefined,
 };
+function getSpawnChance(pixelPos, mouseCanvasPos) {
+	const maxDistance = 20;
+	const x = Math.abs(pixelPos.x - mouseCanvasPos?.x);
+	const y = Math.abs(pixelPos.y - mouseCanvasPos?.y);
+	const distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+	if (distance < maxDistance) return true;
+	return false;
+}
+function init() {
+	let { top, left } = canvas.getBoundingClientRect();
+	canvasPos.top = top;
+	canvasPos.left = left;
+}
 
 function handleMouseMove(e) {
 	mousePos.x = e.clientX;
 	mousePos.y = e.clientY;
 }
-
-document.addEventListener('mousemove', handleMouseMove);
-
-function main() {
-	let canvas = document.getElementById('canvas');
-	let ctx = canvas.getContext('2d');
-	let canvasWidth = canvas.width;
-	let canvasHeight = canvas.height;
-	ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-	let id = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+function getMouseCanvasPos() {
+	const x = mousePos.x - Math.floor(canvasPos.left);
+	const y = mousePos.y - Math.floor(canvasPos.top);
+	let isMouseOverCanvas = true;
+	if (x < 0 || y < 0) isMouseOverCanvas = false;
+	if (x > canvasWidth || y > canvasHeight) isMouseOverCanvas = false;
+	if (isMouseOverCanvas) return { x, y };
+	return null;
+}
+function paint() {
+	const mouseCanvasPos = getMouseCanvasPos();
 	let pixels = id.data;
+	for (let i = 0; i < canvasWidth * canvasHeight; i++) {
+		let x = i % canvasWidth;
+		let y = Math.floor(i / canvasWidth);
+		let chance = getSpawnChance({ x, y }, mouseCanvasPos);
+		let s = chance ? 200 : 0;
 
-	for (let i = 0; i < canvasHeight * canvasWidth; i++) {
 		let off = i * 4;
-		pixels[off] = 0;
-		pixels[off + 1] = 0;
-		pixels[off + 2] = 0;
+		pixels[off] = s;
+		pixels[off + 1] = s;
+		pixels[off + 2] = s;
 		pixels[off + 3] = 255;
 	}
-
 	ctx.putImageData(id, 0, 0);
-
+}
+function main() {
+	init();
+	document.addEventListener('mousemove', handleMouseMove);
+	paint();
 	setInterval(() => {
-		console.log(mousePos);
-	}, 200);
+		paint();
+	}, 1000 / canvasFps);
 }
 
 window.onload = main;
