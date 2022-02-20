995aa5eacdd6b073cec62dd33630fd9c06a83782
Christian Ambukita <christianambukita@gmail.com>
2022-02-20 17:03:19 +0100

    Add canvasWidth state to Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,14 +4,41 @@ import circleAnimation from '../js/circleAnimation';
 import CircleAnim from './CircleAnim';
 import IntroAnim from './IntroAnim';
 
+const maxCanvasWidth = 600;
+
+const getCanvasWidth = (value) => {
+	const flooredValue = value - (value % 100);
+	return flooredValue < maxCanvasWidth ? flooredValue : maxCanvasWidth;
+};
+
 export default function Hero({ scrollElementId }) {
 	const [transition, setTransition] = useState(false);
+	const [canvasWidth, setcanvasWidth] = useState(
+		getCanvasWidth(window.innerWidth)
+	);
+
+	useEffect(() => {
+		function handleResize() {
+			const newWidth = getCanvasWidth(window.innerWidth);
+			setcanvasWidth(newWidth);
+		}
+
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
 
 	return (
 		<header id='header' className='flex-container'>
-			<IntroAnim setTransition={setTransition} transition={transition} />
+			<IntroAnim
+				setTransition={setTransition}
+				transition={transition}
+				canvasWidth={canvasWidth}
+			/>
 			<div className={`transition${transition ? ' visible' : ''}`}>
-				<CircleAnim scrollElementId={scrollElementId} />
+				<CircleAnim
+					scrollElementId={scrollElementId}
+					canvasWidth={canvasWidth}
+				/>
 			</div>
 		</header>
 	);
