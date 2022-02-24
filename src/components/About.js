b446388b2bd74c3af132e5078c9910efe2cc2ac1
Christian Ambukita <christianambukita@gmail.com>
2022-02-24 13:14:40 +0100

    Add content to about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import '../css/About.css';
 
 export default function About() {
 	return (
-		<section id='about' className='flex-container'>
+		<section id='about'>
 			<div className='section-container'>
 				<h2>Who am I?</h2>
-				<article></article>
+				<article>
+					My name is Christian Ambukita and I'm an aspiring front-end developer.
+					I graduated from Poznań University of Technology with a masters degree
+					in Automatic control and Robotics. During my final years of college I
+					realised that coding is what I would like to do as a job. After
+					graduation I started to learn HTML, CSS and JavaScript as I decided to
+					become a front-end web developer. My favorite and mainly used
+					framework is React. This website showcases some of projects I did
+					during my learning process.
+				</article>
 			</div>
 		</section>
 	);
